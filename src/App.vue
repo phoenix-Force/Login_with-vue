bbb2d25c9c4d943d9fbcc9bb93a98110ec8d1b9f
phoenix-Force <paradox.joe.007@gmail.com>
2019-09-11 19:05:24 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input type="text" id="email" class="form-control" v-model="userData.email">
+
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control" v-model="userData.pass">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control" v-model="userData.age">
+                    </div>
+                  <onn-off v-model ="dswitch"></onn-off>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control" v-model="message"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail" v-model="sendmail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail" v-model="sendmail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male" v-model="gender"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female" v-model="gender"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control"
+                            v-model="isSelected">
+                        <option :key='index' dswitchv-for="(p,index) in prioritys">{{p}}</option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary" @click.prevent="done">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail:{{userData.email}}</p>
+                        <p>Password:{{userData.pass}}</p>index
+                        <p>Age:{{userData.age}}</p>
+                        <p style="white-space:pre">Message: {{message}}</p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li :key="index" v-for="(m,index) in sendmail">{{m}}//{{index}}</li>
+                        </ul>
+                        <p>Gender:{{gender}}</p>
+                        <p>Priority:{{isSelected}}</p>
+                        <p>Switched:{{dswitch}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import swtch from "./swirch.vue"
+    export default {
+      data:()=>{
+        return{
+          userData:{
+            email:'',
+            pass:'',
+            age:2
+          },
+          message:'',
+          sendmail:[],
+          gender:'Male',
+          prioritys:['Low','Medium','High'],
+          isSelected:'Medium',
+          dswitch:false
+        }
+      },
+      components:{
+        onnOff:swtch
+      }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/swirch.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <div id="on" :class ="{isactive:value}" @click="dswitch(true)">
+    On
+  </div>
+  <div id="off" :class ="{isactive:!value}" @click="dswitch(false)">
+    Off
+  </div>
+
+</div>
+
+</template>
+<script>
+export default {
+ props:['value'],
+ methods:{
+   dswitch(isOn){
+     this.$emit('input',isOn)
+   }
+
+ }
+
+}
+</script>
+<style scoped>
+#on,#off{
+  height:20px;
+  width:40px;
+  float:left;
+  background-color: lightgray;
+  cursor: pointer;
+  text-align: center;
+
+
+}
+#on.isactive,#on:hover{
+  color:white;
+  background-color: lightgreen;
+}
+#off.isactive,#off:hover{
+  color:white;
+  background-color: lightcoral;
+}
+</style>
